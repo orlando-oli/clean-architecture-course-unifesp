fe0913e11e526e51b2050688838de66ce1e21a9e
Orlando Oliveira <oliorlando6@gmail.com>
2020-12-06 16:59:30 -0300

    test: properly return the user if it was found

--- src/usecases/add-user-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/add-user-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -2,10 +2,25 @@ import { UserData } from '../user-data';
 import { InMemoryUserRepository } from './in-memory-user-repository';
 
 describe('In memory User repository', () => {
-  test('should return null if user was not found', async () => {
+  test('should return null if the user was not found', async () => {
     const users: UserData[] = [];
     const userRepo = new InMemoryUserRepository(users);
+
     const user = await userRepo.findUserByEmail('any@mail.com');
+
     expect(user).toBeNull();
+  });
+
+  test('should return the user if it was found', async () => {
+    const users: UserData[] = [];
+    const name = 'any_name';
+    const email = 'any@mail.com';
+
+    const userRepo = new InMemoryUserRepository(users);
+    await userRepo.add({ name, email });
+
+    const user = await userRepo.findUserByEmail('any@mail.com');
+
+    expect(user.name).toBe(name);
   });
 });
