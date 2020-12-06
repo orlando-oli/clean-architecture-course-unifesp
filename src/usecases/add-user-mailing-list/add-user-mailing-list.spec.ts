70b7f4734e4cd3d4eaaf9986058922845ec8f2ca
Orlando Oliveira <oliorlando6@gmail.com>
2020-12-06 15:32:43 -0300

    test: should add user to the mailing list

---
+++ src/usecases/add-user-mailing-list/add-user-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data';
+
+describe('Add user to the mailing list use case', () => {
+  test('should add user with complete data to the mailing list', async () => {
+    const users: UserData[] = [];
+    console.log(users);
+    // const repo: UserRepo = new InMemoryUserRepository(users);
+    // const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
+    // const name = 'any_name';
+    // const email = 'any@email.com';
+    // const response = await usecase.AddUserToMailingList({ name, email });
+    // const user = repo.findUserByEmail('any@email.com');
+    // expect((await user).name).toBe('any_name');
+  });
+});
