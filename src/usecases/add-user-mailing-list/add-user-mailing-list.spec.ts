e13e9bb4dd9fff9443daf0bbb92d439179c00c0c
Orlando Oliveira <oliorlando6@gmail.com>
2021-01-14 22:49:07 -0300

    test: prevent adding a user with invalid email

--- src/usecases/add-user-mailing-list/add-user-mailing-list.spec.ts
+++ src/usecases/add-user-mailing-list/add-user-mailing-list.spec.ts
@@ -1,3 +1,5 @@
+import { left } from '../../shared/either';
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error';
 import { UserData } from '../../entities/user-data';
 import { AddUserToMailingList } from './add-user-mailing-list';
 import { UserRepository } from './ports/user-repository';
@@ -9,11 +11,27 @@ describe('Add user to the mailing list use case', () => {
 
     const repo: UserRepository = new InMemoryUserRepository(users);
     const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
+
     const name = 'any_name';
     const email = 'any@email.com';
     const response = await usecase.addUserToMailingList({ name, email });
     const user = repo.findUserByEmail('any@email.com');
     expect((await user).name).toBe('any_name');
     expect(response.value.name).toBe('any_name');
+  });
+
+  test('should not add user with invalid email to the mailing list', async () => {
+    const users: UserData[] = [];
+
+    const repo: UserRepository = new InMemoryUserRepository(users);
+    const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
+
+    const name = 'any_name';
+    const invalidEmail = 'invalid_email';
+
+    const response = await usecase.addUserToMailingList({ name, email: invalidEmail });
+    const user = await repo.findUserByEmail('any@email.com');
+    expect(user).toBeNull();
+    expect(response).toEqual(left(new InvalidEmailError()));
   });
 });
