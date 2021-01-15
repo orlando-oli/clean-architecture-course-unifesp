122e25619f45ffa2b50a9dcd73addf40de338119
Orlando Oliveira <oliorlando6@gmail.com>
2021-01-14 22:45:09 -0300

    feat: add valid user to mailing list

---
+++ src/usecases/add-user-mailing-list/add-user-mailing-list.ts
@@ -0,0 +1,31 @@
+/* eslint-disable class-methods-use-this */
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error';
+import { InvalidNameError } from '../../entities/errors/invalid-name-error';
+import { User } from '../../entities/user';
+import { UserData } from '../../entities/user-data';
+import { Either, left, right } from '../../shared/either';
+import { UserRepository } from './ports/user-repository';
+
+export class AddUserToMailingList {
+  private readonly userRepo: UserRepository;
+
+  constructor(userRepo: UserRepository) {
+    this.userRepo = userRepo;
+  }
+
+  public async addUserToMailingList(
+    request: UserData
+  ): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request);
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value);
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request);
+    }
+
+    return right(request);
+  }
+}
