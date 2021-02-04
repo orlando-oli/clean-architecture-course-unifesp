8eba2680bcf969bb113019a7f370962501532a68
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-03 23:21:42 -0300

    feat: return 400 if request has missing parameters

--- src/web-controllers/add-user-controller.ts
+++ src/web-controllers/add-user-controller.ts
@@ -2,6 +2,7 @@
 import { UserData } from '@/entities';
 import { HttpRequest } from '@/web-controllers/ports/http-request';
 import { AddUserToMailingList } from '../usecases';
+import { MissingParamError } from './errors/missing-param-error';
 import { HttpResponse } from './ports';
 import { badRequest, created } from './util/http-helper';
 
@@ -13,6 +14,19 @@ export class AddUserController {
   }
 
   public async handle(request: HttpRequest): Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) {
+      let params = '';
+      if (request.body.name) {
+        params = 'email';
+      } else if (request.body.email) {
+        params = 'name';
+      } else {
+        params = 'name email';
+      }
+
+      return badRequest(new MissingParamError(params));
+    }
+
     const userData: UserData = request.body;
     const response = await this.usecase.addUserToMailingList(userData);
 
