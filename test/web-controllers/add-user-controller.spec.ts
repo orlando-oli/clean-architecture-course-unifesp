9f6fbd6f34dc8138504f042496fb6497bafd4d8d
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-03 23:05:13 -0300

    test: return 400 if request has invalid data

--- test/web-controllers/add-user-controller.spec.ts
+++ test/web-controllers/add-user-controller.spec.ts
@@ -4,9 +4,11 @@ import { UserRepository } from '@/usecases/add-user-mailing-list/ports';
 import { AddUserController } from '@/web-controllers/';
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports/';
 import { InMemoryUserRepository } from '@test/usecases/add-user-mailing-list/repository';
+import { InvalidEmailError } from '../../src/entities/errors/invalid-email-error';
+import { InvalidNameError } from '../../src/entities/errors/invalid-name-error';
 
 describe('Add user web controller', () => {
-  test('shuold return status code 201 if request is valid', async () => {
+  test('should return status code 201 if request is valid', async () => {
     const request: HttpRequest = {
       body: {
         name: 'Any name',
@@ -18,16 +20,16 @@ describe('Add user web controller', () => {
     const repo: UserRepository = new InMemoryUserRepository(users);
     const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
     const controller: AddUserController = new AddUserController(usecase);
-    const responde: HttpResponse = await controller.handle(request);
+    const response: HttpResponse = await controller.handle(request);
 
-    expect(responde.statusCode).toEqual(201);
-    expect(responde.body).toEqual(request.body);
+    expect(response.statusCode).toEqual(201);
+    expect(response.body).toEqual(request.body);
   });
 
-  test("shuold return status code 400 if request isn't valid", async () => {
+  test('should return status code 400 if request has an invalid name', async () => {
     const request: HttpRequest = {
       body: {
-        name: 'Any name',
+        name: 'A',
         email: 'any@mail.com',
       },
     };
@@ -36,9 +38,27 @@ describe('Add user web controller', () => {
     const repo: UserRepository = new InMemoryUserRepository(users);
     const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
     const controller: AddUserController = new AddUserController(usecase);
-    const responde: HttpResponse = await controller.handle(request);
+    const response: HttpResponse = await controller.handle(request);
 
-    expect(responde.statusCode).toEqual(201);
-    expect(responde.body).toEqual(request.body);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('should return status code 400 if request has an invalid email', async () => {
+    const request: HttpRequest = {
+      body: {
+        name: 'Any name',
+        email: 'invalid_mail.com',
+      },
+    };
+    const users: UserData[] = [];
+
+    const repo: UserRepository = new InMemoryUserRepository(users);
+    const usecase: AddUserToMailingList = new AddUserToMailingList(repo);
+    const controller: AddUserController = new AddUserController(usecase);
+    const response: HttpResponse = await controller.handle(request);
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidEmailError);
   });
 });
