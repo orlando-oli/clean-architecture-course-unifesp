bff7af0fd20d42548130af2cb25f9119b43bb81e
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-03 23:40:36 -0300

    feat: should return 500 if response raises

--- src/web-controllers/add-user-controller.ts
+++ src/web-controllers/add-user-controller.ts
@@ -1,41 +1,45 @@
 /* eslint-disable consistent-return */
 import { UserData } from '@/entities';
 import { HttpRequest } from '@/web-controllers/ports/http-request';
-import { AddUserToMailingList } from '../usecases';
+import { UseCase } from '../usecases/ports/use-cases';
 import { MissingParamError } from './errors/missing-param-error';
 import { HttpResponse } from './ports';
-import { badRequest, created } from './util/http-helper';
+import { badRequest, created, serverError } from './util/http-helper';
 
 export class AddUserController {
-  private readonly usecase: AddUserToMailingList;
+  private readonly usecase: UseCase;
 
-  constructor(usecase: AddUserToMailingList) {
+  constructor(usecase: UseCase) {
     this.usecase = usecase;
   }
 
   public async handle(request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) {
-      let params = '';
-      if (request.body.name) {
-        params = 'email';
-      } else if (request.body.email) {
-        params = 'name';
-      } else {
-        params = 'name email';
+    try {
+      if (!request.body.name || !request.body.email) {
+        let params = '';
+        if (request.body.name) {
+          params = 'email';
+        } else if (request.body.email) {
+          params = 'name';
+        } else {
+          params = 'name email';
+        }
+
+        return badRequest(new MissingParamError(params));
       }
 
-      return badRequest(new MissingParamError(params));
-    }
+      const userData: UserData = request.body;
+      const response = await this.usecase.perform(userData);
 
-    const userData: UserData = request.body;
-    const response = await this.usecase.addUserToMailingList(userData);
+      if (response.isRight()) {
+        return created(response.value);
+      }
 
-    if (response.isRight()) {
-      return created(response.value);
-    }
-
-    if (response.isLeft()) {
-      return badRequest(response.value);
+      if (response.isLeft()) {
+        return badRequest(response.value);
+      }
+    } catch (error) {
+      return serverError(error);
     }
   }
 }
