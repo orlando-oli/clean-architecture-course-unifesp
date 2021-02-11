232909ec42913697c684781d491a5d9bd1ff1d63
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-10 23:43:30 -0300

    test: add bodyParser, content-type, cors

---
+++ test/main/config/middleware/body-parser.test.ts
@@ -0,0 +1,11 @@
+import { app } from '@/main/config';
+import request from 'supertest';
+
+describe('Body parser middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body);
+    });
+    await request(app).post('/test_body_parser').send({ name: 'Name' }).expect({ name: 'Name' });
+  });
+});
