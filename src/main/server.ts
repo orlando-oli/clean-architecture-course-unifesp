5578fa1ce756a3984ecfff3b750eb3171ceba2ae
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-20 20:43:32 -0300

    feat: add mongodb connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,9 @@
 import 'module-alias/register';
+import { MongoHelper } from '@/external/repositories/mongodb/helper';
 import { app } from '@/main/config/app';
 
-app.listen(5000, () => {
-  console.log('Server running at https://localhost:5000');
+MongoHelper.connect('mongodb://localhost').then(async () => {
+  app.listen(5000, () => {
+    console.log('Server running at https://localhost:5000');
+  });
 });
