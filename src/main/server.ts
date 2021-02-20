eb78f53a291fea701c66ac1f35d887345d1cc1ed
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-20 20:46:39 -0300

    feat: add catch statement to server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,11 @@
-import 'module-alias/register';
 import { MongoHelper } from '@/external/repositories/mongodb/helper';
 import { app } from '@/main/config/app';
+import 'module-alias/register';
 
-MongoHelper.connect('mongodb://localhost').then(async () => {
-  app.listen(5000, () => {
-    console.log('Server running at https://localhost:5000');
-  });
-});
+MongoHelper.connect('mongodb://localhost')
+  .then(async () => {
+    app.listen(5000, () => {
+      console.log('Server running at https://localhost:5000');
+    });
+  })
+  .catch(console.error);
