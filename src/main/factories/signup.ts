b5c8358101454456cb73da5527ec0fd925b1360c
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-20 20:44:14 -0300

    feat: choose the right repo based on NODE_ENV

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,11 +1,14 @@
-import { InMemoryUserRepository } from '../../usecases/add-user-mailing-list/repository/in-memory-user-repository';
-import { AddUserToMailingList } from '../../usecases/add-user-mailing-list/add-user-mailing-list';
-import { AddUserController } from '../../web-controllers/add-user-controller';
+import { MongodbUserRepository } from '@/external/repositories/mongodb/mongodb-user-repository';
+import { AddUserToMailingList } from '@/usecases/add-user-mailing-list/add-user-mailing-list';
+import { InMemoryUserRepository } from '@/usecases/add-user-mailing-list/repository/in-memory-user-repository';
+import { AddUserController } from '@/web-controllers/add-user-controller';
 
 export const makeAddUserController = (): AddUserController => {
-  const inMemoryUserRepository = new InMemoryUserRepository([]);
+  let repo;
+  if (process.env.NODE_ENV === 'test') repo = new InMemoryUserRepository([]);
+  else repo = new MongodbUserRepository();
 
-  const AddUserToMailingListUseCase = new AddUserToMailingList(inMemoryUserRepository);
+  const AddUserToMailingListUseCase = new AddUserToMailingList(repo);
 
   return new AddUserController(AddUserToMailingListUseCase);
 };
