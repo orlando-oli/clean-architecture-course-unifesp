77dad9155de4b1fe59c369f5e36cbad4c9d97f58
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-20 20:41:21 -0300

    chore: config jest-mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,13 @@
  */
 
 module.exports = {
+  preset: '@shelf/jest-mongodb',
   clearMocks: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/test/**', '!**/config/**'],
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/test'],
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
