21bea20eca9897f6331b90b0d92cef0ab3b9fd0e
Orlando Oliveira <oliorlando6@gmail.com>
2021-02-26 01:40:07 -0300

    fix: fix signup route test without workarounds

--- test/main/routes/signup-route.test.ts
+++ test/main/routes/signup-route.test.ts
@@ -1,7 +1,20 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper';
 import { app } from '@/main/config';
 import request from 'supertest';
 
 describe('signup route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    await MongoHelper.clearCollection('users');
+  });
+
   test('should return an user on success', async () => {
     app.post('/test_cors', (req, res) => {
       res.send();
